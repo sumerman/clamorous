42b335bd3a413368b813eb1c62aa640e979e8b8d
Meleshkin Valery <nekro.grave@gmail.com>
2012-06-05 13:20:13 +0400

    sender util fix

--- utils/send.sh
+++ utils/send.sh
@@ -2,10 +2,9 @@
 ADDR="http://localhost:8080/clamorous/publish"
 if [ -n "$1" ]; then
 	ADDR=$1
-	echo "bar"
 fi
 
 while read LN; do
-	echo $LN | curl -XPOST $ADDR --data-binary $LN
+	echo $LN | curl -XPOST $ADDR -d @-
 	echo ""
 done
